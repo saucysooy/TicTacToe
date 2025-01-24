dcc7cb171f407b54682942e1fc3cc39e71f3fafc
saucysooy <saucysooy@hotmail.com>
2025-01-24 17:37:39 +0100

    feat: Add update game state logic handling game state real-time
    refactor: Update game logic to use Firebase Firestore
    
    This commit introduces a new logic that will handle the board's state when it changes from the firestore and adapting the code to utilize Game class for real-time updates and state management, including handling game, moves, and status changes"

--- app/src/main/java/com/example/tictactoemain/GameViewModel.kt
+++ app/src/main/java/com/example/tictactoemain/GameViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.tictactoemain
 
+import android.util.Log
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.launch
 
 class GameViewModel : ViewModel(){
 
@@ -20,58 +24,124 @@ class GameViewModel : ViewModel(){
         DIAGONAL_2(listOf(2, 4, 6));
     }
 
-    private val _boardState = mutableStateListOf<MutableList<String>>(
-        mutableListOf("", "", ""),
-        mutableListOf("", "", ""),
-        mutableListOf("", "", "")
-    )
-    val boardState: List<List<String>> = _boardState
+    enum class GameStatus {
+        ONGOING,
+        COMPLETED,
+        DRAW
+    }
 
     private val _currentPlayer = mutableStateOf("X")
     val currentPlayer: State<String> = _currentPlayer
 
-    private val _gameStatus = mutableStateOf("ongoing")
-    val gameStatus: State<String> = _gameStatus
+    private val _gameStatus = mutableStateOf(GameStatus.ONGOING)
+    val gameStatus: State<GameStatus> = _gameStatus
 
     private val _winner = mutableStateOf<String>("")
     val winner: State<String?> = _winner
 
+    private val db = Firebase.firestore
+    private var gameId: String = ""
+    private var playerId: String = ""
+
+    private val _isMyTurn = mutableStateOf(false)
+    val isMyTurn: State<Boolean> = _isMyTurn
+
+    private val _game = mutableStateOf(Game())
+    val game: State<Game> = _game
+
+
+    private fun updateGameState(game: Game) {
+        // Update current player
+        _currentPlayer.value = game.currentPlayerTurn
+
+        // Update game status
+        _gameStatus.value = when (game.gameStatus) {
+            "ongoing" -> GameStatus.ONGOING
+            "completed" -> GameStatus.COMPLETED
+            "draw" -> GameStatus.DRAW
+            else -> GameStatus.ONGOING
+        }
+
+        // Update isMyTurn
+        val mySymbol = if (playerId == game.player1) "X" else "O"
+        _isMyTurn.value = game.currentPlayerTurn == mySymbol
+
+        //update winner
+        if (game.gameStatus == "completed") {
+            _winner.value = if (game.currentPlayerTurn == "X") "O" else "X"
+        } else if (game.gameStatus == "draw") {
+            _winner.value = "draw"
+        } else {
+            _winner.value = ""
+        }
+    }
+
     fun makeMove(row: Int, col: Int) {
-        if (_boardState[row][col] == "" && _gameStatus.value == "ongoing") {
+        val boardState = _game.value.boardState
+        val rowKey = row.toString()
+        val currentRow = boardState[rowKey]
+
+        if (_isMyTurn.value && currentRow?.getOrNull(col) == "" && _gameStatus.value == GameStatus.ONGOING) {
             // Create a new list with the updated row
-            val newRow = _boardState[row].toMutableList()
+            val newBoardState = boardState.toMutableMap()
+            val newRow = currentRow.toMutableList()
             newRow[col] = _currentPlayer.value
-            _boardState[row] = newRow
+            newBoardState[rowKey] = newRow
 
-            checkWin()
+            // Update the game state in Firestore
+            val updatedGame = _game.value.copy(
+                boardState = newBoardState,
+                currentPlayerTurn = if (_currentPlayer.value == "X") "O" else "X"
+            )
 
-            // Switch the player if the game is still ongoing
-            if (_gameStatus.value == "ongoing") {
-                _currentPlayer.value = if (_currentPlayer.value == "X") "O" else "X"
+            checkWin(updatedGame)
+
+            viewModelScope.launch {// making this tied to the lifecycle
+                // just in case the user closes down the app ensuring the process stops on the firestore side
+                db.collection("games").document(gameId)
+                    .set(updatedGame)
+                    .addOnSuccessListener {
+                        Log.d("GameViewModel", "Document successfully written!")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("GameViewModel", "Error writing document", e)
+                    }
             }
         }
     }
 
-    private fun checkWin() {
+    private fun checkWin(game: Game) {
         for (combination in WinningCombination.values()) {
-            val firstCell = getCellValue(combination.indices[0])
+            val firstCell = getCellValue(combination.indices[0], game)
 
             // Check if the first cell is not empty and all cells in the combination match the first cell
             if (firstCell.isNotEmpty() && combination.indices.all { index ->
-                    getCellValue(index) == firstCell
+                    getCellValue(index, game) == firstCell
                 }) {
-                _winner.value = firstCell
-                _gameStatus.value = "completed"
+                // Update the game state in Firestore
+                val updatedGame = game.copy(
+                    gameStatus = "completed"
+                )
+                viewModelScope.launch {
+                    db.collection("games").document(gameId)
+                        .set(updatedGame)
+                        .addOnSuccessListener {
+                            Log.d("GameViewModel", "Document successfully written!")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("GameViewModel", "Error writing document", e)
+                        }
+                }
                 return
             }
         }
         // Check if the game is a draw after checking for a win
-        checkDraw()
+        checkDraw(game)
     }
 
-    private fun checkDraw() {
+    private fun checkDraw(game: Game) {
         var isDraw = true
-        for (row in _boardState) {
+        for (row in game.boardState.values) {
             for (cell in row) {
                 if (cell.isEmpty()) {
                     isDraw = false
@@ -82,21 +152,35 @@ class GameViewModel : ViewModel(){
         }
 
         if (isDraw) {
-            _gameStatus.value = "draw"
+            // Update the game state in Firestore
+            val updatedGame = game.copy(
+                gameStatus = "draw"
+            )
+            viewModelScope.launch {
+                db.collection("games").document(gameId)
+                    .set(updatedGame)
+                    .addOnSuccessListener {
+                        Log.d("GameViewModel", "Document successfully written!")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("GameViewModel", "Error writing document", e)
+                    }
+            }
         }
     }
 
-    private fun getCellValue(index: Int): String {
+
+    private fun getCellValue(index: Int, game: Game): String {
         return when (index) {
-            0 -> _boardState[0][0]
-            1 -> _boardState[0][1]
-            2 -> _boardState[0][2]
-            3 -> _boardState[1][0]
-            4 -> _boardState[1][1]
-            5 -> _boardState[1][2]
-            6 -> _boardState[2][0]
-            7 -> _boardState[2][1]
-            8 -> _boardState[2][2]
+            0 -> game.boardState["0"]?.get(0) ?: ""
+            1 -> game.boardState["0"]?.get(1) ?: ""
+            2 -> game.boardState["0"]?.get(2) ?: ""
+            3 -> game.boardState["1"]?.get(0) ?: ""
+            4 -> game.boardState["1"]?.get(1) ?: ""
+            5 -> game.boardState["1"]?.get(2) ?: ""
+            6 -> game.boardState["2"]?.get(0) ?: ""
+            7 -> game.boardState["2"]?.get(1) ?: ""
+            8 -> game.boardState["2"]?.get(2) ?: ""
             else -> ""
         }
     }
